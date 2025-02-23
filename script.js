6b431ded388531157d92c78f8a584e54e7463d35
Viva1981 <stuller.zsolt@gmail.com>
2025-02-23 19:21:15 +0100

    reméljük a végső

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 document.getElementById('search').addEventListener('keyup', function() {
     let filter = document.getElementById('search').value.toLowerCase();
     let docItems = document.querySelectorAll('.doc-item');
+    let found = false;
 
     docItems.forEach(function(item) {
         let text = item.textContent.toLowerCase();
         if (text.includes(filter)) {
             item.style.display = '';
+            found = true;
         } else {
             item.style.display = 'none';
         }
     });
+
+    let message = document.getElementById("no-results");
+    if (!found) {
+        message.style.display = "block";
+    } else {
+        message.style.display = "none";
+    }
 });
